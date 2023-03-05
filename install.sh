cbda00305b2a22cb1e0f21ca97d5568bf2f3aa4a
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 10:18:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 echo "Hello:)"
 echo "this script created by RezkyMC"
-echo "pls wait install requirement"
+read -p "You choose to install or not to install? (yes/no) " yn
+
+case $yn in 
+
+	yes ) echo ok, we will proceed;;	
+  no ) echo exiting...;
+
+		exit;;
+
+	* ) echo invalid response;
+
+		exit 1;;
+
+esac
+
+echo doing stuff...
 pkg upgrade && pkg update -y
 termux-setup-storage 
 cd /sdcard && mkdir TmuxMC
