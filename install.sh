243b2e71696d747b247138ec9ed282b37052baf1
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 10:09:29 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Hello:)"
+echo "this script created by RezkyMC"
+echo "pls wait install requirement"
+pkg upgrade && pkg update -y
+termux-setup-storage 
+cd /sdcard && mkdir TmuxMC
+cd TmuxMC
