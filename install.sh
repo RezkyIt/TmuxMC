c6fc96d2c13ee8cad3bbf5460ad26d09bcecfd41
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 11:03:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,4 +42,4 @@ echo "1.14.4 - https://api.papermc.io/v2/projects/paper/versions/1.14.4/builds/2
 echo "1.12.2 - https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar"
 echo "1.8.8 - https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar"
 read -p "choose you Server Version: " Version
-wget -O $Version
+wget -O server.jar $Version
