b7cf3242cbee75647049772a7556533d7f0eddd7
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 10:31:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-echo "Hello:)"
-echo "this script created by RezkyMC"
+echo "===Hello:)==="
+echo "===this script created by RezkyMC==="
+echo "===Minecraft-Server-Termux-By-RezkyMC==="
 read -p "You choose to install or not to install? (yes/no) " yn
 
 case $yn in 
@@ -9,9 +10,13 @@ case $yn in
     * ) echo invalid response;
         exit 1;;
 esac
-echo Wait a moment...
-
+echo ===Wait a moment...===
 pkg upgrade && pkg update -y
 termux-setup-storage 
 cd /sdcard && mkdir TmuxMC
 cd TmuxMC
+clear
+echo ===Install Java 17===
+pkg install openjdk-17-jdk -y
+
+
