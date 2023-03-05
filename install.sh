c88d09f95dbaae6670308b37631eb48809316924
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 10:46:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-echo "===Hello:)==="
-echo "===this script created by RezkyMC==="
-echo "===Minecraft-Server-Termux-By-RezkyMC==="
+echo "===================================="
+echo "Hello:)"
+echo "this script created by RezkyMC"
+echo "Minecraft-Server-Termux-By-RezkyMC"
+echo "===================================="
 read -p "You choose to install or not to install? (yes/no) " yn
 
 case $yn in 
@@ -10,17 +12,23 @@ case $yn in
     * ) echo invalid response;
         exit 1;;
 esac
-echo "===Wait a moment...==="
+echo "======================"
+echo "...Wait a moment..."
+echo "======================"
 pkg upgrade && pkg update -y
 pkg install wget -y
 termux-setup-storage 
 cd /sdcard && mkdir TmuxMC
 cd TmuxMC
 clear
-echo "===Install Java 17==="
+echo "======================"
+echo "Install Java 17"
+echo "======================"
 pkg install openjdk-17 -y
 wget -O java17 https://download.java.net/java/GA/jdk17.0.1/2a2082e5a09d4267845be086888add4f/12/GPL/openjdk-17.0.1_linux-aarch64_bin.tar.gz
-echo "===Java 17 Installed==="
+echo "======================"
+echo "Java 17 Installed"
+echo "======================"
 clear
 echo "===Select Minecraft Server Version==="
 echo "===1.19.1/2(4-8GB-RAM)==="
