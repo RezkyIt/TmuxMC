2b1968228b469a1596befc73c593f444faf993f2
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-05 10:21:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,14 @@ echo "this script created by RezkyMC"
 read -p "You choose to install or not to install? (yes/no) " yn
 
 case $yn in 
-
-	yes ) echo ok, we will proceed;;	
-  no ) echo exiting...;
-
-		exit;;
-
-	* ) echo invalid response;
-
-		exit 1;;
-
+    yes ) echo ok, we will install;;	
+    no ) echo exiting...;
+        exit;;
+    * ) echo invalid response;
+        exit 1;;
 esac
+echo Wait a moment...
 
-echo doing stuff...
 pkg upgrade && pkg update -y
 termux-setup-storage 
 cd /sdcard && mkdir TmuxMC
