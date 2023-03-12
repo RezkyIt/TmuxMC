d03de61004225ebf756c5011d8349e596e52eb84
RezkyMC <126237354+RezkyIt@users.noreply.github.com>
2023-03-12 10:47:27 +0700

    Delete runNGROK.sh

--- runNGROK.sh
+++
@@ -1,6 +0,0 @@
-./ngrok tcp 25565 > &>/dev/null &
-java -Xms1G -Xmx1G -jar server.jar
-echo "================"
-echo "you ip server"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "================"
